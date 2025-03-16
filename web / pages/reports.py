3e9e90a872c78c604eb5a5ddb5cbde6a05e829be
TAMER ELGOHARY <EGYADMIN@users.noreply.huggingface.co>
2025-03-16 01:51:08 +0000

    Delete web

--- web / pages/reports.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_reports():
-    st.title("التقارير")
-    st.write("هذه الصفحة مخصصة لإنشاء وتحليل التقارير الخاصة بالمناقصات وسلاسل الإمداد.")
--- web / pages/success_prediction.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_success_prediction():
-    st.title("توقع احتمالية النجاح")
-    st.write("هذه الصفحة مخصصة لتحليل وتوقع فرص نجاح المناقصات والمشاريع.")
