fe9a4fccba52022384d7a370d4f7fde180ef875d
TAMER ELGOHARY <EGYADMIN@users.noreply.huggingface.co>
2025-03-16 01:30:41 +0000

    Update web/pages/__init__.py

--- web/pages/__init__.py
+++ web/pages/__init__.py
@@ -1,6 +1,1 @@
-# هذا الملف يجعل مجلد pages وحدة Python
-# يمكن استخدامه لاستيراد الدوال المهمة لجعلها متاحة بشكل مباشر
-
-from .home import show_home_page
-
-__all__ = ['show_home_page']
+# تهيئة مجلد web كموديول في Python
